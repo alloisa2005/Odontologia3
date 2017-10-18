648ae1912e056bee7fab8f4bc9ea853b2da97777
User <User@DESKTOP-6HCC4M1>
2017-10-18 10:29:50 -0300

    Prueba nueva

--- src/GUI/frmVerFactura.java
+++ src/GUI/frmVerFactura.java
@@ -6,24 +6,45 @@
 package GUI;
 
 import Controladores.Conexion;
+import IO.Consulta;
 import IO.Factura;
 import IO.LineaFactura;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.print.PageFormat;
+import java.awt.print.Paper;
+import java.awt.print.Printable;
+import static java.awt.print.Printable.NO_SUCH_PAGE;
+import static java.awt.print.Printable.PAGE_EXISTS;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
+import javax.imageio.ImageIO;
 
 /**
  *
  * @author User
  */
-public class frmVerFactura extends javax.swing.JDialog {
+public class frmVerFactura extends javax.swing.JDialog implements Printable{
 
     SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    Factura facAux = new Factura();
+    List<LineaFactura> lineas = null;
     
     public frmVerFactura(javax.swing.JDialog parent, boolean modal, Factura factura) {
         super(parent, modal);
         initComponents();        
+        
+        facAux = factura;   // La guardo en facAux para dsp poder imprimir                
+        
+        lineas = facAux.getLineaFacturas();
         
         this.setTitle("Factura Nro.: " + factura.getNumero());
 
@@ -89,6 +110,89 @@ public class frmVerFactura extends javax.swing.JDialog {
         
     }
     
+    @Override
+    public int print(Graphics g, PageFormat f, int pageIndex) throws PrinterException {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        int x;
+        String s;                
+        
+        Date fchFactura = facAux.getFecha();
+        if(pageIndex == 0){            
+//            
+//            Graphics2D g2d = (Graphics2D) g;
+//            g2d.translate(x, 40);
+//            g2d.scale(0.65, 0.65); //Reducción de la impresión al 75%
+//            tblPacientes.printAll(g);
+            
+            try {
+               Image img = ImageIO.read(new File("src/Imagenes/factura.png").toURI().toURL());
+                g.drawImage(img, 5, 5, 590, 410, null);
+            } catch (Exception ex) {
+            }
+            
+            // Fecha (Dia)
+            g.setFont(new Font("Calibri", Font.BOLD, 11));            
+            s = String.valueOf(fchFactura.getDate());
+            g.drawString(s, 418, 76);
+            
+            // Fecha (Mes)
+            g.setFont(new Font("Calibri", Font.BOLD, 11));   
+            int mes = fchFactura.getMonth();
+            s = DevuelvoMes(mes);
+            //s = String.valueOf();
+            g.drawString(s, 465, 76);
+            
+            // Fecha (Anio)
+            g.setFont(new Font("Calibri", Font.BOLD, 11));               
+            s = String.valueOf(fchFactura.getYear()+ 1900);    // el getYear me devuelve en int el año atual menos 1900, por eso le sumo 1900
+            //s = String.valueOf();
+            g.drawString(s, 544, 76);                                    
+            
+            // Nro Factura
+            g.setFont(new Font("Calibri", Font.BOLD, 11)); 
+            g.setColor(Color.red);
+            s = facAux.getNumero();
+            g.drawString(s, 500, 30);
+            
+            // Nombre y Apellido de paciente
+            g.setFont(new Font("Calibri", Font.BOLD, 11)); 
+            g.setColor(Color.red);
+            s = facAux.getPaciente().toString();  
+            //s = String.valueOf();
+            g.drawString(s, 150, 128); 
+            
+            // Cédula del paciente
+            g.setFont(new Font("Calibri", Font.BOLD, 11));  
+            g.setColor(Color.red);
+            s = facAux.getPaciente().getId();  
+            //s = String.valueOf();
+            g.drawString(s, 400, 128); 
+            
+            //double monto = 0.0;
+            
+            for (int i = 0; i < lineas.size(); i++) {
+                LineaFactura linea = lineas.get(i);
+                
+                g.setColor(Color.black);
+                s = linea.getConsulta().getTitulo() + " - " + df.format(linea.getConsulta().getFecha());
+                x = 210 + (i*25);
+                g.drawString(s, 50, x); 
+                
+                s = String.valueOf(linea.getMonto()); //linea.getConsulta().getPagos().get(linea.getConsulta().getPagos().size()-1).getMonto().toString();
+                g.drawString(s, 508, x);
+                //monto += linea.getConsulta().getPagos().get(linea.getConsulta().getPagos().size()-1).getMonto();
+            }
+            
+            s = String.valueOf(facAux.getMonto());  //txtPago.getText();
+            g.drawString(s, 508, 380);
+            
+            return PAGE_EXISTS;
+        }else{            
+            return NO_SUCH_PAGE;
+        }
+    }
+    
     public String DevuelvoMes(int nroMes){
         String mes = "";
         
@@ -110,6 +214,26 @@ public class frmVerFactura extends javax.swing.JDialog {
         return mes;
     }
     
+    public void Imprimo(){           
+           
+           PrinterJob job = PrinterJob.getPrinterJob();
+           Paper hoja = new Paper();
+           hoja.setImageableArea(0, 0, 595, 841);
+           hoja.setSize(595, 841);
+           try {
+              // Diálogo para elegir el formato de impresión
+              PageFormat pageFormat = new PageFormat();
+              pageFormat.setPaper(hoja);
+              
+             // job.setPrintable(new ImpFact(listaConsultas,paciete), pageFormat);
+              job.setPrintable(this, pageFormat);
+              //pageFormat = job.pageDialog(pageFormat);
+              if (job.printDialog()) {
+                  job.print();
+              }
+           } catch (Exception e) {
+           }    
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -121,6 +245,7 @@ public class frmVerFactura extends javax.swing.JDialog {
 
         lblNroFactura = new javax.swing.JLabel();
         lblPaciente = new javax.swing.JLabel();
+        brnImprimir = new javax.swing.JButton();
         lblCedula = new javax.swing.JLabel();
         lblMontoTotal = new javax.swing.JLabel();
         lblMes = new javax.swing.JLabel();
@@ -160,6 +285,17 @@ public class frmVerFactura extends javax.swing.JDialog {
         getContentPane().add(lblPaciente);
         lblPaciente.setBounds(260, 170, 340, 40);
 
+        brnImprimir.setFont(new java.awt.Font("Calibri", 1, 17)); // NOI18N
+        brnImprimir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/imprimir.png"))); // NOI18N
+        brnImprimir.setText("Re Imprimir");
+        brnImprimir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                brnImprimirActionPerformed(evt);
+            }
+        });
+        getContentPane().add(brnImprimir);
+        brnImprimir.setBounds(440, 650, 180, 60);
+
         lblCedula.setFont(new java.awt.Font("Calibri", 1, 19)); // NOI18N
         lblCedula.setForeground(new java.awt.Color(255, 0, 0));
         lblCedula.setText("jLabel1");
@@ -172,7 +308,7 @@ public class frmVerFactura extends javax.swing.JDialog {
         lblMontoTotal.setText("jLabel1");
         lblMontoTotal.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
         getContentPane().add(lblMontoTotal);
-        lblMontoTotal.setBounds(860, 550, 110, 40);
+        lblMontoTotal.setBounds(850, 550, 130, 40);
 
         lblMes.setFont(new java.awt.Font("Calibri", 1, 19)); // NOI18N
         lblMes.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -269,53 +405,21 @@ public class frmVerFactura extends javax.swing.JDialog {
         getContentPane().add(lblFactura);
         lblFactura.setBounds(12, 13, 1035, 616);
 
-        setSize(new java.awt.Dimension(1077, 689));
+        setSize(new java.awt.Dimension(1077, 770));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void brnImprimirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_brnImprimirActionPerformed
+        Imprimo();
+    }//GEN-LAST:event_brnImprimirActionPerformed
 
     /**
      * @param args the command line arguments
      */
-//    public static void main(String args[]) {
-//        /* Set the Nimbus look and feel */
-//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-//         */
-//        try {
-//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-//                if ("Nimbus".equals(info.getName())) {
-//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-//                    break;
-//                }
-//            }
-//        } catch (ClassNotFoundException ex) {
-//            java.util.logging.Logger.getLogger(frmVerFactura.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (InstantiationException ex) {
-//            java.util.logging.Logger.getLogger(frmVerFactura.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (IllegalAccessException ex) {
-//            java.util.logging.Logger.getLogger(frmVerFactura.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-//            java.util.logging.Logger.getLogger(frmVerFactura.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        }
-//        //</editor-fold>
-//
-//        /* Create and display the dialog */
-//        java.awt.EventQueue.invokeLater(new Runnable() {
-//            public void run() {
-//                frmVerFactura dialog = new frmVerFactura(new javax.swing.JFrame(), true);
-//                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-//                    @Override
-//                    public void windowClosing(java.awt.event.WindowEvent e) {
-//                        System.exit(0);
-//                    }
-//                });
-//                dialog.setVisible(true);
-//            }
-//        });
-//    }
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton brnImprimir;
     private javax.swing.JLabel lblAnio;
     private javax.swing.JLabel lblCedula;
     private javax.swing.JLabel lblDia;
